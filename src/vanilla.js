bfdee549d78ef8c996d16c9e8cf27434c0e6911a
samuel21695 <samuel21695@gmail.com>
2023-11-26 00:18:48 +0900

    ✨ vanilla React component

---
+++ src/vanilla.js
@@ -0,0 +1,13 @@
+// 바닐라 JavaScript를 사용하여 React 컴포넌트 정의
+function App() {
+  return document.createElement('div');
+}
+
+// 렌더링 함수
+function render(component, container) {
+  container.innnerHTML = ''; // 컨테이너 비우기
+  container.appendChild(component()); //컴포넌트 렌더링
+}
+
+// 렌더링 실행
+render(App, document.getElementById('root'));
